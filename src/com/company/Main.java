dbc73cea8647a1ecdb7f1bcec451e50f99b1068d
kataraktax <marek.biedrzycki1984@gmail.com>
2019-05-01 23:15:30 +0200

    start

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        EventQueue.invokeLater(()->{
+            JFrame frame = new JFrame();
+            frame.add(new CalculatorPanel());
+            frame.setTitle("Calculator");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+            frame.setSize(300, 400);
+        });
     }
 }
